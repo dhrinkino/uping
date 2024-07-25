620a5889518292d45327e3b80622424fd40c592d
Dominik Hrinkino <admin@hrinkino.com>
2024-07-25 16:39:09 +0200

    Added UDP IPv4 support

---
+++ udp.h
@@ -0,0 +1,10 @@
+//
+// Created by Dominik Hrinkino on 25/07/2024.
+//
+
+#ifndef UDP_H
+#define UDP_H
+#include <tins/ip.h>
+Tins::IP udp(std::string src_ipv4, std::string dest_ipv4, int src_port, int dst_port, int size);
+
+#endif //UDP_H
