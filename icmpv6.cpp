1fc74b27617b923759b6d1737382b18cdcb11e21
Dominik Hrinkino <admin@hrinkino.com>
2024-07-25 15:18:41 +0200

    ICMPv6 packet generator

---
+++ icmpv6.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Dominik Hrinkino on 25/07/2024.
+//
+
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <tins/tins.h>
+#include "generator.h"
+
+using namespace std;
+using namespace Tins;
+
+    IPv6 icmpv6(std::string src, std::string dest, int size) {
+        // define SRC and DEST
+        std::string source = src;
+        std::string destination = dest;
+
+        std::string data = generate(size);
+
+        // compose ICMPv6 packet
+        IPv6 pkt_ip = IPv6(destination, source) / ICMPv6();
+        ICMPv6& icmpv6 = pkt_ip.rfind_pdu<ICMPv6>();
+        icmpv6.type(ICMPv6::ECHO_REQUEST);
+        icmpv6.code(0);
+
+        // set TTL to 128 (like OS Windows)
+        pkt_ip.hop_limit(128);
+
+        // insert data payload
+        RawPDU pkt_data(data);
+        pkt_ip /= pkt_data;
+        return pkt_ip;
+    }
