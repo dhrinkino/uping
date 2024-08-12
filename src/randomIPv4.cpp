f9a751cccb432b5ba0511adb784c457634bc1390
dhrinkino <admin@hrinkino.com>
2024-08-12 20:31:08 +0200

    Fixed bug, generateIPv4 only returns valid routeable IPv4 address

--- src/randomIPv4.cpp
+++ src/randomIPv4.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
-#include <iomanip>
 #include <sstream>
 #include <random>
 
 std::string randomIPv4() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint8_t> dis(0, 255);
 
+    std::vector<uint8_t> firstOctetRanges;
+
+    // push to one vector all possible first octets
+    for (uint8_t i = 1; i <= 9; ++i) {
+        firstOctetRanges.push_back(i);
+    }
+    for (uint8_t i = 11; i <= 126; ++i) {
+        firstOctetRanges.push_back(i);
+    }
+    for (uint8_t i = 128; i <= 168; ++i) {
+        firstOctetRanges.push_back(i);
+    }
+    for (uint8_t i = 173; i <= 191; ++i) {
+        firstOctetRanges.push_back(i);
+    }
+    for (uint8_t i = 193; i <= 223; ++i) {
+        firstOctetRanges.push_back(i);
+    }
+
+    std::uniform_int_distribution<size_t> firstOctetDist(0, firstOctetRanges.size() - 1);
+    uint8_t firstOctet = firstOctetRanges[firstOctetDist(gen)];
+
+    std::uniform_int_distribution<uint8_t> dis(0, 255);
     uint8_t octets[4];
-    for (int i = 0; i < 4; ++i) {
+    octets[0] = firstOctet; // force first octet
+    for (int i = 1; i < 4; ++i) {
         octets[i] = dis(gen);
     }
 
-    // merge together
     std::stringstream ss;
     ss << static_cast<int>(octets[0]);
     for (int i = 1; i < 4; ++i) {
